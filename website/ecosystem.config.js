928d08f16404965c819be28519b017b246eec9df
YanceyOfficial <yanceyofficial@gmail.com>
2023-05-08 21:05:43 +0800

    feat(wip): add landing website

--- website/ecosystem.config.js
+++ website/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'hyperchat-website',
-        script: 'node_modules/.bin/next start',
+        script: 'node_modules/.bin/next start -p 2999',
         watch: true,
         autorestart: true,
         max_memory_restart: '1G',
