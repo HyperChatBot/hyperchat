d5ec27a7779fc6a89b7461dbad4951dd555f6f3e
YanceyOfficial <yanceyofficial@gmail.com>
2023-07-12 17:05:38 +0800

    style: format code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,28 +1,28 @@
 import useAppData from './useAppData'
+import useAudio from './useAudio'
 import useChatCompletion from './useChatCompletion'
 import useCompany from './useCompany'
 import useDB from './useDB'
 import useImageGeneration from './useImageGeneration'
 import useMessages from './useMessages'
-import useRequest from './useRequest'
 import useOnline from './useOnline'
 import useOpenAI from './useOpenAI'
-import useAudio from './useAudio'
+import useRequest from './useRequest'
 import useSettings from './useSettings'
 import useTextCompletion from './useTextCompletion'
 import useTheme from './useTheme'
 
 export {
   useAppData,
+  useAudio,
   useChatCompletion,
   useCompany,
   useDB,
   useImageGeneration,
   useMessages,
-  useRequest,
   useOnline,
   useOpenAI,
-  useAudio,
+  useRequest,
   useSettings,
   useTextCompletion,
   useTheme
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -6,7 +6,8 @@ export const OPENAI_CHAT_COMPLETION_URL = OPENAI_BASE_URL + '/chat/completions'
 
 export const OPENAI_TEXT_COMPLETION_URL = OPENAI_BASE_URL + '/completions'
 
-export const OPENAI_IMAGE_GENERATION_URL = OPENAI_BASE_URL + '/images/generations'
+export const OPENAI_IMAGE_GENERATION_URL =
+  OPENAI_BASE_URL + '/images/generations'
 
 export const EMPTY_CHAT_HINT = 'Create your first conversation!'
 
