886d158c3569a996900a5271427672c9b0764a42
YanceyOfficial <yanceyofficial@gmail.com>
2023-07-23 00:09:44 +0800

    feat: some adjustment for configurations

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,41 +1,10 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  // tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true
-  },
-  // to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ['VITE_', 'TAURI_'],
-  build: {
-    // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG
-  },
   resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src')
-    },
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
     mainFields: ['module', 'jsnext:main', 'jsnext']
-  },
-  esbuild: {
-    supported: {
-      'top-level-await': true
-    }
   }
 })
